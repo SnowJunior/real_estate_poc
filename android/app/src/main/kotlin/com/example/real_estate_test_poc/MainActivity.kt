2bd14aa56644e69133259587168047382d6a084c
George Onyango <onyangogeorge954@gmail.com>
2024-10-13 19:15:29 +0300

    chore: clear flutter boilerplate

---
+++ android/app/src/main/kotlin/com/example/real_estate_test_poc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.real_estate_test_poc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:real_estate_test_poc/app.dart';
+
+void main() {
+  runApp(const RealEstateApp());
+}
